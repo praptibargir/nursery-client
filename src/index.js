534537ed3215e640a902e371908fd5d92e720165
Prapti Bargir <praptibargir21@gmail.com>
2024-08-02 19:16:07 +0530

    frontend designed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import Home from './views/Home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<Home/>
+  },
+  {
+    path:"*",
+    element:<h1>404 Not Found</h1>
+  }
+])
+
 root.render(
-  <h1>Nursery Client</h1>
+  <div>
+    <RouterProvider router={router}/>
+    
+  </div>
 );
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "./Home.css"
+import PlantCard from '../../components/PlantCard/PlantCard'
+import axios from 'axios'
+import { Toaster } from 'react-hot-toast';
+import toast from 'react-hot-toast'
 
 function Home() {
+
+    const[plant,setPlant]=useState([])
+
+    const loadPlants=async()=>{
+        toast.loading("Loading Plants.....")
+        const response=await axios.get(`http://localhost:8000/plants`)
+        toast.dismiss()
+        toast.success("Plants loaded successfully..")
+        setPlant(response.data.data)
+    }
+
+    useEffect(()=>{
+        loadPlants()
+    },[])
+
   return (
-    <div>Home</div>
+    <div>
+        <h1>Plants</h1>
+        {
+            plant.map((plant,i)=>{
+                const {_id,
+                    name,
+                    category,
+                    image,
+                    price,
+                    description}=plant
+                return <PlantCard 
+                key={i}
+                _id={_id} 
+                name={name} 
+                category={category} 
+                image={image} 
+                price={price} 
+                description={description}/>
+            })
+        }
+        <Toaster/>
+    </div>
+    
   )
 }
 
